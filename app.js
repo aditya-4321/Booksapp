7881eada5d1919f6a69ef60c97452bd1a7947adb
Aditya Tomar <hashstar212@gmail.com>
2019-02-24 16:17:13 +0000

    third commit

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 var express=require("express"),
     app=express()
+
+//Array of objects   
+var book=[
+    {
+        name:"Infinity Countdown",
+        author:"Gerry Gudda",
+        image:"https://cdn1.thr.com/sites/default/files/2017/12/infinitycountdown-p_2017.jpg"
+    },
+    {
+       name:"Infinity Countdown",
+       author:"Gerry Gudda",
+        image:"https://cdn1.thr.com/sites/default/files/2017/12/infinitycountdown-p_2017.jpg" 
+    },
+    {
+        name:"Infinity Countdown",
+        author:"Gerry Gudda",
+        image:"https://cdn1.thr.com/sites/default/files/2017/12/infinitycountdown-p_2017.jpg",
+    },
+    {
+        name:"Infinity Countdown",
+        author:"Gerry Gudda",
+        image:"https://cdn1.thr.com/sites/default/files/2017/12/infinitycountdown-p_2017.jpg"
+    } ,{
+        name:"Infinity Countdown",
+        author:"Gerry Gudda",
+        image:"https://cdn1.thr.com/sites/default/files/2017/12/infinitycountdown-p_2017.jpg"
+    },
+    {
+       name:"Infinity Countdown",
+       author:"Gerry Gudda",
+        image:"https://cdn1.thr.com/sites/default/files/2017/12/infinitycountdown-p_2017.jpg" 
+    },
+    {
+        name:"Infinity Countdown",
+        author:"Gerry Gudda",
+        image:"https://cdn1.thr.com/sites/default/files/2017/12/infinitycountdown-p_2017.jpg",
+    }
     
-app.get("/books",function(req, res){
-    res.send("books are our best friends");
+]
+    
+
+app.get("/",function(req, res){
+    res.render("books.ejs",{book: book})
 })   
-    
-    
-    
+
     
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Server has Started");
